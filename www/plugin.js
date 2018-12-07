896d1232d46cad875972900405efc97941181fde
Unknown <mikakrooswijk@live.nl>
2018-12-07 14:44:57 +0100

    removed fat arrow function for better compatibility

--- www/plugin.js
+++ www/plugin.js
@@ -2,27 +2,27 @@
 
 function mongodb() { }
 
-mongodb.prototype.initiate = (appId) => {
+mongodb.prototype.initiate = function(appId){
     return call('MongoDBStorage', 'initiate', [appId]);
 };
 
-mongodb.prototype.insertOne = (database, collection, document) => {
+mongodb.prototype.insertOne = function(database, collection, document){
     return call('MongoDBStorage', 'insertOne', [database, collection, document]);
 };
 
-mongodb.prototype.findOne = (database, collection, criteria) => {
+mongodb.prototype.findOne = function(database, collection, criteria){
     return call('MongoDBStorage', 'findOne', [database, collection, criteria]);
 };
 
-mongodb.prototype.replaceOne = (database, collection, criteria, update) => {
+mongodb.prototype.replaceOne = function(database, collection, criteria, update){
     return call('MongoDBStorage', 'replaceOne', [database, collection, criteria, update]);
 };
 
-mongodb.prototype.findAll = (database, collection) => {
+mongodb.prototype.findAll = function(database, collection){
     return call('MongoDBStorage', 'findAll', [database, collection]);
 };
 
-mongodb.prototype.deleteOne = (database, collection, criteria) => {
+mongodb.prototype.deleteOne = function(database, collection, criteria){
     return call('MongoDBStorage', 'deleteOne', [database, collection, criteria]);
 };
 
@@ -37,6 +37,6 @@ cordova.addConstructor(mongodb.install);
 
 function call(className, action, options) {
     return new Promise((resolve, reject) => {
-        cordova.exec((returnSuccess) => { resolve(returnSuccess); }, (returnError) => { reject(returnError); }, className, action, options);
+        cordova.exec(function (returnSuccess){ resolve(returnSuccess); }, function(returnError){ reject(returnError); }, className, action, options);
     });
 }
